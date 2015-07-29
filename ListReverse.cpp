4a38480946afb8c3cde17b75b891943f18db0326
April Cheny <april.chey@gmail.com>
2015-07-29 15:33:52 +0800

    v2
    增加递归版本逆序函数

--- ListReverse.cpp
+++ ListReverse.cpp
@@ -28,6 +28,47 @@ Node* reverse(Node* head)
 	return node;
 }
 
+/*
+ *递归版本
+ *举例：假设输入1->2->3->4->5->null，则首次调整为4->5->3->1->2->null，然后再递归调整4->5和1->2
+ */
+
+Node* reverse2(Node* head, int num)
+{
+	int loop_count = num >> 1;
+	if (num <= 1)
+		return head;
+	if (num == 2)
+	{
+		swap(head->val, head->next->val);
+		return head;
+	}
+
+	Node* n1 = head;
+	Node* n2 = head;
+	int count1 = (num % 2 == 0 ? loop_count - 1 : loop_count);
+	while (count1 >= 0)
+	{
+		n2 = n2->next;
+		count1--;
+	}
+
+	Node* n3 = n2;
+	int count2 = loop_count;
+	while (count2 > 0)
+	{
+		swap(n1->val, n3->val);
+		n1 = n1->next;
+		n3 = n3->next;
+		count2--;
+	}
+
+	reverse2(head, num >> 1);
+	reverse2(n2, num >> 1);
+
+	return head;
+}
+
 int main()
 {
 	const int num = 5;
